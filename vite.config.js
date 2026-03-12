be00ee52cc2802a0f9455a23a48e7560fcaa0db9
card3d-studio <otis@card3d-studio.com>
2026-03-12 16:02:56 -0400

    Add embed test page to Vite build

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,14 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
   base: '/mmes-3d-logo/',
-});
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        embedTest: resolve(__dirname, 'embed-test.html'),
+      },
+    },
+  },
+})
